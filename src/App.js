b04b4d528b4d03ccbf930b7fc66185d30d6ff833
sumedharkulkarni <sumedha.rk.05@gmail.com>
2020-10-17 22:25:52 +0530

    Added background and highlight

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [allQuoteData, setAllQuoteData] = useState([]);
   const [quote, setQuote] = useState('');
   const [show, setView] = useState(false);
+  const [imageUrl, setImageUrl] = useState('');
+  const [allImageUrls, setAllImageUrl] = useState([]);
 
   useEffect(() => {
     fetch('https://type.fit/api/quotes')
@@ -18,11 +20,31 @@ function App() {
         setAllQuoteData(data);
         setView(true);
       });
+
+    const requestObj = {
+      method: 'GET',
+      headers: {
+        Authorization:
+          'Bearer ' +
+          '563492ad6f917000010000018d5b2480f3ee47ce90c02f6098dfab71',
+      },
+    };
+    fetch('https://api.pexels.com/v1/search?query=nature', requestObj)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAllImageUrl(data.photos);
+        setImageUrl(data.photos[0].src.landscape);
+      });
   }, []);
 
   const onShuffleClick = () => {
     setView(false);
     const randomLength = Math.floor(Math.random() * (allQuoteData.length - 1));
+    const randomImgLength = Math.floor(
+      Math.random() * (allImageUrls.length - 1)
+    );
+    setImageUrl(allImageUrls[randomImgLength].src.landscape);
     setQuote(allQuoteData[randomLength]);
     setTimeout(() => {
       setView(true);
@@ -31,25 +53,32 @@ function App() {
 
   return (
     show && (
-      <div className="landing-page">
-        <div className="quote">
-          <h1>
-            <img
-              className="quote-sign"
-              src={leftQuoteIcon}
-              alt="leftQuoteIcon"
-            />
-            {quote.text}
-            <img
-              className="quote-sign"
-              src={rightQuoteIcon}
-              alt="rightQuoteIcon"
-            />
-          </h1>
+      <div
+        className="landing-page"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      >
+        <div class="highlight-container">
+          <div class="highlight">
+            <div className="quote">
+              <h1>
+                <img
+                  className="quote-sign"
+                  src={leftQuoteIcon}
+                  alt="leftQuoteIcon"
+                />
+                {quote.text}
+                <img
+                  className="quote-sign"
+                  src={rightQuoteIcon}
+                  alt="rightQuoteIcon"
+                />
+              </h1>
+            </div>
+            <p className="author">
+              {quote.author === '' ? 'Anonymous' : quote.author}
+            </p>
+          </div>
         </div>
-        <h2 className="author">
-          {quote.author === '' ? 'Anonymous' : quote.author}
-        </h2>
         <button onClick={onShuffleClick} className="shuffle-button">
           <img src={shuffleIcon} alt="shuffleIcon" />
           <span>Shuffle</span>
